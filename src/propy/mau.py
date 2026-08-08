c0155b337692af3cf515ae5f8668284b157339d1
Gijs Bruining <gijs@flying-fish.tech>
2026-08-08 21:39:07 +0200

    Added the mau3-35 polynomials

--- src/propy/mau.py
+++ src/propy/mau.py
@@ -80,3 +80,51 @@ class MAUPropeller(Propeller):
     @cached_property
     def kq(self) -> Callable[[ScalarOrArray], NDArray[float64]]:
         return lambda j: array(j)
+
+    @property
+    def _kt3_35(self) -> NDArray[float64]:
+        return array([
+            - 0.0674536 * self.pd_ratio**0 +
+            + 0.5892164 * self.pd_ratio**1 +
+            - 0.1971395 * self.pd_ratio**2 +
+            + 0.0521675 * self.pd_ratio**3,
+
+            - 0.0751015 * self.pd_ratio**0 +
+            - 0.3569108 * self.pd_ratio**1 +
+            + 0.2951303 * self.pd_ratio**2 +
+            - 0.0967409 * self.pd_ratio**3,
+
+            - 0.3041660 * self.pd_ratio**0 +
+            + 0.0901521 * self.pd_ratio**1 +
+            + 0.3234639 * self.pd_ratio**2 +
+            - 0.1886770 * self.pd_ratio**3,
+
+            + 0.1063169 * self.pd_ratio**0 +
+            - 0.1536279 * self.pd_ratio**1 +
+            - 0.0067481 * self.pd_ratio**2 +
+            + 0.0488185 * self.pd_ratio**3,
+        ], dtype=float64)
+
+    @property 
+    def _kq3_35(self) -> NDArray[float64]:
+        return array([
+            - 0.00176065 * self.pd_ratio**0 +
+            + 0.02144957 * self.pd_ratio**1 +
+            + 0.01563138 * self.pd_ratio**2 +
+            + 0.01464783 * self.pd_ratio**3,
+
+            - 0.00183090 * self.pd_ratio**0 +
+            - 0.01321039 * self.pd_ratio**1 +
+            - 0.02596498 * self.pd_ratio**2 +
+            + 0.00833566 * self.pd_ratio**3,
+
+            - 0.00895205 * self.pd_ratio**0 +
+            + 0.02653115 * self.pd_ratio**1 +
+            - 0.00510880 * self.pd_ratio**2 +
+            - 0.00761559 * self.pd_ratio**3,
+
+            - 0.03270370 * self.pd_ratio**0 +
+            + 0.02510493 * self.pd_ratio**1 +
+            - 0.00052158 * self.pd_ratio**2 +
+            - 0.00142337 * self.pd_ratio**3,
+        ], dtype=float64)
